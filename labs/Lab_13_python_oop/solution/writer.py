8c3837eed259d079d211a7a8051c7e031b8ecc42
Danilzyk03 <danil.zykov.2020@mail.ru>
2023-12-28 12:56:31 +0300

    Labs

--- labs/Lab_13_python_oop/solution/writer.py
+++ labs/Lab_13_python_oop/solution/writer.py
@@ -1,15 +1,14 @@
 import xlsxwriter
 from datetime import datetime
 from base import BaseXlsBlock
-from blocks import PayersBlock, CitiesBlock, BankAccountBlock, HeaderBlock
-
+from block import HeaderBlock, QuartetPaymentBlock, CustomerGeographyBlock, BankAccountBlock
 
 class XlsAnalyticPaymentWriter:
     ANALYTICS_BLOCKS_CLASSES = [
         HeaderBlock,
-        PayersBlock,
-        CitiesBlock,
-        BankAccountBlock
+        QuartetPaymentBlock,
+        CustomerGeographyBlock,
+        BankAccountBlock,
     ]
 
     def __init__(self, data):
@@ -17,14 +16,19 @@ class XlsAnalyticPaymentWriter:
 
     def write_excel_report(self, output_file):
         workbook = xlsxwriter.Workbook(output_file)
-        worksheet = workbook.add_worksheet()
+        worksheet = workbook.add_worksheet('analytics')
+        worksheet.set_column(0, 0, 40)
+        worksheet.set_column(1, 5, 40)
+
         row = 0
         col = 0
 
-        for i in range(0, len(self.ANALYTICS_BLOCKS_CLASSES)):
-            worksheet.set_column(i, i, 30)
-            block_instance = self.ANALYTICS_BLOCKS_CLASSES[i](worksheet, workbook, row, col, self.data)
-            block_instance.write_header()
-            block_instance.write_data()
+        for block_class in self.ANALYTICS_BLOCKS_CLASSES:
+            block_instance = block_class(worksheet, workbook, row, col, self.data)
+            block_instance.writeHeaderCol()
+            block_instance.writeData()
+            if row == 0:
+                row -= 9
+            row += 14
 
         workbook.close()
